40e6ea49c10e66cfa7a531ae89d1aa0c14cbda66
Excelsies <gme97531@gmail.com>
2020-11-10 18:34:46 -0700

    Adjusted how the background video loads

--- Website/digital-portfolio/src/components/HeroSection.js
+++ Website/digital-portfolio/src/components/HeroSection.js
@@ -4,10 +4,23 @@ import { Button } from './Button'
 
 import './HeroSection.css'
 
+const Video = () => {
+    const src = getVideoSrc(window.innerWidth);
+    return(
+        <video muted="true" src={src} autoPlay loop playsInline/>
+    );
+}
+
+const getVideoSrc = width => {
+    if(width >= 1080) return "/DigitalPortfolio/videos/backgroundVideo.mp4"
+    if(width >= 720) return "/DigitalPortfolio/videos/backgroundVideoTablet.mp4"
+    return "/DigitalPortfolio/videos/backgroundVideoMobile.mp4"
+}
+
 function HeroSection() {
     return (
         <div className='hero-container'>
-            <video muted="true" src="/DigitalPortfolio/videos/backgroundVideo.mp4" autoPlay loop/>
+            <Video />
             <h1>Garet Eyre</h1>
             <p>A website about me!</p>
             <div className="hero-btns">
